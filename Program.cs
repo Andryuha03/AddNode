76a2692fef83e182599a0d4893b2a51c7864e97b
Andryuha03 <andryusha2315@bk.ru>
2025-04-16 15:46:49 +0800

    Реализованы действия для пользователя: Создание файла для заметок и добавление заметок. Посмотреть и удалить пока недоступны.

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Security.Cryptography;
+while (true)
+{
+    List<string> strings = new List<string>();
+    string? anywords;
+
+    Console.WriteLine("Выберите одно из действий:\n* Создать файл для заметок [0]\n* Добавить заметку [1] \n* Посмотреть все заметки [2]\n* Удалить заметку [3] \n* Выйти [4]");
+    int Choose = Convert.ToInt32(Console.ReadLine());
+    if (Choose == 4)
+        break;
+    switch (Choose)
+    {
+        case 0:
+            string MyDocuments = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            string fileName = "txt.txt";
+            string filePath = Path.Combine(MyDocuments, fileName);
+
+
+            break;
+        case 1:
+
+            Console.WriteLine("Оставьте заметку");
+            anywords = Console.ReadLine();
+            strings.Add(anywords);
+            break;
+        case 2:
+            foreach (string s in strings)
+            {
+                Console.Write(strings.Count);
+                Console.WriteLine(s);
+            }
+            break;
+        case 3:
+
+            break;
+        case 4:
+
+            break;
+    }
+}
